0ea011dec77838c5a474c2452dee900a7296a07d
Julienne San Luis <julienne.codes@gmail.com>
2021-07-05 01:31:03 -0400

    added footer component by creating footer.js file in src/components directory, used footer component in index and about pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "../components/footer";
 
 export default function About(){
     return(
         <section className="about-page">
             <h1>About</h1>
             <p>This is the about page.</p>
+            <Footer />
         </section>
     )
 }
